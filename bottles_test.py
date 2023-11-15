3ba243a3de10d7fce299cb7e59c4bd1eeca31d5b
Katrina Owen <katrina.owen@gmail.com>
2023-11-15 10:10:56 +0100

    verse 3 test FAILS

--- bottles_test.py
+++ bottles_test.py
@@ -9,3 +9,13 @@ def test_the_first_verse():
     '98 bottles of milk on the wall.\n'
   )
   assert Bottles().verse(99) == expected
+
+
+def test_another_verse():
+  expected = (
+    '3 bottles of milk on the wall, '
+    '3 bottles of milk.\n'
+    'Take one down and pass it around, '
+    '2 bottles of milk on the wall.\n'
+  )
+  assert Bottles().verse(3) == expected
