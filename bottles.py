9b177ad5c44cd91ee20cf37b6590b24164f67e15
Katrina Owen <katrina.owen@gmail.com>
2023-11-15 11:00:56 +0100

    revert to outer conditional

--- bottles.py
+++ bottles.py
@@ -1,8 +1,15 @@
 class Bottles:
   def verse(self, number):
+    if (number == 2):
+      return (
+        '2 bottles of milk on the wall, '
+        '2 bottles of milk.\n'
+        'Take one down and pass it around, '
+        '1 bottle of milk on the wall.\n'
+      )
     return (
       f'{number} bottles of milk on the wall, '
       f'{number} bottles of milk.\n'
       f'Take one down and pass it around, '
-      f'{number-1} bottle{"s" if number-1 != 1 else ""} of milk on the wall.\n'
+      f'{number-1} bottles of milk on the wall.\n'
     )
