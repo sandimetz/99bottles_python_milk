294fd3628efcffccef980dc052597b43c47ba0ea
Katrina Owen <katrina.owen@gmail.com>
2024-01-04 09:14:42 +0100

    delete obsolete 2 case

--- bottles.py
+++ bottles.py
@@ -21,13 +21,6 @@ class Bottles:
           'Take it down and pass it around, '
           'no more bottles of milk on the wall.\n'
         )
-      case 2:
-        return (
-          f'{number} bottles of milk on the wall, '
-          f'{number} bottles of milk.\n'
-          f'Take one down and pass it around, '
-          f'{number-1} {self.container(number-1)} of milk on the wall.\n'
-        )
       case _:
         return (
           f'{number} bottles of milk on the wall, '
