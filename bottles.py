991d8e0a2cdd03860c023fe232f129aff56ddf0c
Katrina Owen <katrina.owen@gmail.com>
2024-01-04 13:08:00 +0100

    send action from verse

--- bottles.py
+++ bottles.py
@@ -12,7 +12,7 @@ class Bottles:
           f'{self.quantity(number).capitalize()} {self.container(number)}'
           ' of milk on the wall, '
           f'{self.quantity(number)} {self.container(number)} of milk.\n'
-          f'Go to the store and buy some more, '
+          f'{self.action(number)}, '
           f'99 bottles of milk on the wall.\n'
         )
       case _:
@@ -20,7 +20,7 @@ class Bottles:
           f'{self.quantity(number).capitalize()} {self.container(number)}'
           ' of milk on the wall, '
           f'{self.quantity(number)} {self.container(number)} of milk.\n'
-          f'Take {self.pronoun(number)} down and pass it around, '
+          f'{self.action(number)}, '
           f'{self.quantity(number-1)} {self.container(number-1)}'
           ' of milk on the wall.\n'
         )
