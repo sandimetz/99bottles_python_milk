c5c3d51c254f893c24e89d4045cf56d27d6afff4
Katrina Owen <katrina.owen@gmail.com>
2024-01-04 13:32:43 +0100

    try common template

--- bottles.py
+++ bottles.py
@@ -6,6 +6,15 @@ class Bottles:
     return '\n'.join(self.verse(i) for i in reversed(range(lower, upper + 1)))
 
   def verse(self, number):
+    return (
+      f'{self.quantity(number).capitalize()} {self.container(number)}'
+      ' of milk on the wall, '
+      f'{self.quantity(number)} {self.container(number)} of milk.\n'
+      f'{self.action(number)}, '
+      f'{self.quantity(self.successor(number))} '
+      f'{self.container(self.successor(number))}'
+      ' of milk on the wall.\n'
+    )
     match number:
       case 0:
         return (
