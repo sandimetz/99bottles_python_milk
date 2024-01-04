bf33946efaf645e8c4f70a98d69401379f9efb70
Katrina Owen <katrina.owen@gmail.com>
2024-01-04 11:54:31 +0100

    pass arg to quantity in case 1 phrase 4

--- bottles.py
+++ bottles.py
@@ -19,7 +19,7 @@ class Bottles:
           f'{number} {self.container(number)} of milk on the wall, '
           f'{number} {self.container(number)} of milk.\n'
           f'Take {self.pronoun(number)} down and pass it around, '
-          f'{self.quantity()} bottles of milk on the wall.\n'
+          f'{self.quantity(number-1)} bottles of milk on the wall.\n'
         )
       case _:
         return (
