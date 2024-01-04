e46fee465e47631d8289ea7e153d08e1b8b0e00f
Katrina Owen <katrina.owen@gmail.com>
2024-01-04 11:20:04 +0100

    send pronoun in default case

--- bottles.py
+++ bottles.py
@@ -25,7 +25,7 @@ class Bottles:
         return (
           f'{number} {self.container(number)} of milk on the wall, '
           f'{number} {self.container(number)} of milk.\n'
-          f'Take one down and pass it around, '
+          f'Take {self.pronoun()} down and pass it around, '
           f'{number-1} {self.container(number-1)} of milk on the wall.\n'
         )
 
