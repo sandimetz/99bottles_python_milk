94b2cc755b95389e364f94f104c26a830d2fcf66
Katrina Owen <katrina.owen@gmail.com>
2024-01-04 08:57:30 +0100

    send container in default

--- bottles.py
+++ bottles.py
@@ -33,7 +33,7 @@ class Bottles:
           f'{number} bottles of milk on the wall, '
           f'{number} bottles of milk.\n'
           f'Take one down and pass it around, '
-          f'{number-1} bottles of milk on the wall.\n'
+          f'{number-1} {self.container()} of milk on the wall.\n'
         )
 
   def container(self):
