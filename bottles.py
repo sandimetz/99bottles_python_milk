8490fb099fb6d93572ff8096e712b917d347e289
Katrina Owen <katrina.owen@gmail.com>
2024-01-05 11:58:31 +0100

    delete obsolete successor implementation

--- bottles.py
+++ bottles.py
@@ -30,9 +30,6 @@ class Bottles:
 
   def successor(self, number):
     return BottleNumber(number).successor(number)
-    if number == 0:
-      return 99
-    return number - 1
 
 
 class BottleNumber:
