603b1e4685d92292a69a0d8a3e437420e2f60947
Katrina Owen <katrina.owen@gmail.com>
2024-01-05 11:54:21 +0100

    parse and execute BottleNumber container

--- bottles.py
+++ bottles.py
@@ -20,6 +20,7 @@ class Bottles:
     return BottleNumber(number).quantity(number)
 
   def container(self, number):
+    BottleNumber(number).container(number)
     if number == 1:
       return 'bottle'
     return 'bottles'
