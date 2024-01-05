9e01399dd9344d4a8cd179041296ed9ec7ed9cc2
Katrina Owen <katrina.owen@gmail.com>
2024-01-05 13:55:21 +0100

    stop passing argument to successor in sender

--- bottles.py
+++ bottles.py
@@ -26,7 +26,7 @@ class Bottles:
     return BottleNumber(number).action()
 
   def successor(self, number):
-    return BottleNumber(number).successor(number)
+    return BottleNumber(number).successor()
 
 
 class BottleNumber:
