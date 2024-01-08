926a43a12ffcce45ba207daa9be979b6be9c982d
Katrina Owen <katrina.owen@gmail.com>
2024-01-08 14:25:20 +0100

    use new factory for next bottle number in verse

--- bottles.py
+++ bottles.py
@@ -7,7 +7,7 @@ class Bottles:
 
   def verse(self, number):
     bottle_number = BottleNumber.given(number)
-    next_bottle_number = self.bottle_number_given(bottle_number.successor())
+    next_bottle_number = BottleNumber.given(bottle_number.successor())
     # next_bottle_number = bottle_number.successor()
 
     return (
