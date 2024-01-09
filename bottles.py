80fb0dbb82fc68635ecbb046aa434a4b0b2d0666
Katrina Owen <katrina.owen@gmail.com>
2024-01-09 12:03:23 +0100

    pass successor to quantity in phrase 4 case 0

--- bottles.py
+++ bottles.py
@@ -13,7 +13,7 @@ class Bottles:
           ' of milk on the wall, '
           f'{self.quantity(number)} {self.container(number)} of milk.\n'
           f'{self.action(number)}, '
-          f'{self.quantity(99)} {self.container(number-1)}'
+          f'{self.quantity(self.successor(number))} {self.container(number-1)}'
           ' of milk on the wall.\n'
         )
       case _:
