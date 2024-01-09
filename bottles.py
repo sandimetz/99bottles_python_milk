a9a02e6130a987e38ef11dd03b8011b224b6c239
Katrina Owen <katrina.owen@gmail.com>
2024-01-09 08:54:59 +0100

    Add speculatively general example

--- bottles.py
+++ bottles.py
@@ -2,15 +2,62 @@ class Bottles:
   def song(self):
     return self.verses(99, 0)
 
-  def verses(self, hi, lo):
-    return '\n'.join(self.verse(n) for n in range(hi, lo - 1, -1))
-
-  def verse(self, n):
-    return (
-      f'{"No more" if n == 0 else n} bottle{"s" if n != 1 else ""}'
-      f' of milk on the wall, '
-      f'{"no more" if n == 0 else n} bottle{"s" if n != 1 else ""} of milk.\n' +
-      (f'Go to the store and buy some more, ' if n == 0 else f'Take {"one" if n != 1 else "it"} down and pass it around, ') +
-      f'{"99" if n - 1 < 0 else ("no more" if n - 1 == 0 else n - 1)} bottle{"s" if n - 1 != 1 else ""}'
-      f' of milk on the wall.\n'
+  def verses(self, high, low):
+    return '\n'.join(
+      self.verse(verse_number) for verse_number in sorted(range(low, high+1), reverse=True)
     )
+
+  def verse(self, number):
+    return self.verse_for(number).text()
+
+  def verse_for(self, number):
+    def no_more(verse):
+      return (
+        'No more bottles of milk on the wall, '
+        'no more bottles of milk.\n'
+        'Go to the store and buy some more, '
+        '99 bottles of milk on the wall.\n'
+      )
+
+    def last_one(verse):
+      return (
+        '1 bottle of milk on the wall, '
+        '1 bottle of milk.\n'
+        'Take it down and pass it around, '
+        'no more bottles of milk on the wall.\n'
+      )
+
+    def penultimate(verse):
+      return (
+        '2 bottles of milk on the wall, '
+        '2 bottles of milk.\n'
+        'Take one down and pass it around, '
+        '1 bottle of milk on the wall.\n'
+      )
+
+    def default(verse):
+      return (
+        f'{verse.number} bottles of milk on the wall, '
+        f'{verse.number} bottles of milk.\n'
+        f'Take one down and pass it around, '
+        f'{verse.number - 1} bottles of milk on the wall.\n'
+      )
+
+    lyric_functions = [no_more, last_one, penultimate, default]
+    chosen_lyric = (
+      lyric_functions[0] if number == 0 else
+      lyric_functions[1] if number == 1 else
+      lyric_functions[2] if number == 2 else
+      lyric_functions[3]
+    )
+
+    return Verse(number, chosen_lyric)
+
+
+class Verse:
+  def __init__(self, number, lyrics):
+    self.number = number
+    self.lyrics = lyrics
+
+  def text(self):
+    return self.lyrics(self)
