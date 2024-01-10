sandimetz/99bottles_python

pass 2 test with inner conditional

bottles.py
@@ -1,15 +1,8 @@
 class Bottles:
   def verse(self, number):
-    if (number == 2):
-      return (
-        '2 bottles of beer on the wall, '
-        '2 bottles of beer.\n'
-        'Take one down and pass it around, '
-        '1 bottle of beer on the wall.\n'
-      )
     return (
       f'{number} bottles of beer on the wall, '
       f'{number} bottles of beer.\n'
       f'Take one down and pass it around, '
-      f'{number-1} bottles of beer on the wall.\n'
+      f'{number-1} bottle{"s" if number-1 != 1 else ""} of beer on the wall.\n'
     )

sandimetz/99bottles_python_milk

bottles.py
@@ -1,15 +1,8 @@
 class Bottles:
   def verse(self, number):
-    if (number == 2):
-      return (
-        '2 bottles of milk on the wall, '
-        '2 bottles of milk.\n'
-        'Take one down and pass it around, '
-        '1 bottle of milk on the wall.\n'
-      )
     return (
       f'{number} bottles of milk on the wall, '
       f'{number} bottles of milk.\n'
       f'Take one down and pass it around, '
-      f'{number-1} bottles of milk on the wall.\n'
+      f'{number-1} bottle{"s" if number-1 != 1 else ""} of milk on the wall.\n'
     )