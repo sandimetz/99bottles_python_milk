sandimetz/99bottles_python

pass verses test #1 w/literal

bottles.py
@@ -1,4 +1,17 @@
 class Bottles:
+  def verses(self, _a, _b):
+    return (
+      '99 bottles of beer on the wall, '
+      '99 bottles of beer.\n'
+      'Take one down and pass it around, '
+      '98 bottles of beer on the wall.\n'
+      '\n'
+      '98 bottles of beer on the wall, '
+      '98 bottles of beer.\n'
+      'Take one down and pass it around, '
+      '97 bottles of beer on the wall.\n'
+    )
+
   def verse(self, number):
     match number:
       case 0:

sandimetz/99bottles_python_milk

bottles.py
@@ -1,4 +1,17 @@
 class Bottles:
+  def verses(self, _a, _b):
+    return (
+      '99 bottles of milk on the wall, '
+      '99 bottles of milk.\n'
+      'Take one down and pass it around, '
+      '98 bottles of milk on the wall.\n'
+      '\n'
+      '98 bottles of milk on the wall, '
+      '98 bottles of milk.\n'
+      'Take one down and pass it around, '
+      '97 bottles of milk on the wall.\n'
+    )
+
   def verse(self, number):
     match number:
       case 0: