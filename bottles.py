sandimetz/99bottles_python

pass verse 99 test

bottles.py
@@ -1,3 +1,8 @@
 class Bottles:
   def verse(self, _):
-    pass
+    return (
+      '99 bottles of beer on the wall, '
+      '99 bottles of beer.\n'
+      'Take one down and pass it around, '
+      '98 bottles of beer on the wall.\n'
+    )

sandimetz/99bottles_python_milk

bottles.py
@@ -1,3 +1,8 @@
 class Bottles:
   def verse(self, _):
-    pass
+    return (
+      '99 bottles of milk on the wall, '
+      '99 bottles of milk.\n'
+      'Take one down and pass it around, '
+      '98 bottles of milk on the wall.\n'
+    )
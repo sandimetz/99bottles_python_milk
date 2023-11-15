c0539743d358fff50c3dfc8cd61378ee6936a170
Katrina Owen <katrina.owen@gmail.com>
2023-11-15 10:06:09 +0100

    pass verse 99 test

--- bottles.py
+++ bottles.py
@@ -1,3 +1,8 @@
 class Bottles:
   def verse(self, _):
-    pass
+    return (
+      '99 bottles of milk on the wall, '
+      '99 bottles of milk.\n'
+      'Take one down and pass it around, '
+      '98 bottles of milk on the wall.\n'
+    )
