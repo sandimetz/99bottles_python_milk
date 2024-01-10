sandimetz/99bottles_python

pass 3 and 99 with conditional

bottles.py
@@ -1,8 +1,15 @@
 class Bottles:
-  def verse(self, _):
+  def verse(self, number):
+    if (number == 99):
+      return (
+        '99 bottles of beer on the wall, '
+        '99 bottles of beer.\n'
+        'Take one down and pass it around, '
+        '98 bottles of beer on the wall.\n'
+      )
     return (
-      '99 bottles of beer on the wall, '
-      '99 bottles of beer.\n'
+      '3 bottles of beer on the wall, '
+      '3 bottles of beer.\n'
       'Take one down and pass it around, '
-      '98 bottles of beer on the wall.\n'
+      '2 bottles of beer on the wall.\n'
     )

sandimetz/99bottles_python_milk

bottles.py
@@ -1,8 +1,15 @@
 class Bottles:
-  def verse(self, _):
+  def verse(self, number):
+    if (number == 99):
+      return (
+        '99 bottles of milk on the wall, '
+        '99 bottles of milk.\n'
+        'Take one down and pass it around, '
+        '98 bottles of milk on the wall.\n'
+      )
     return (
-      '99 bottles of milk on the wall, '
-      '99 bottles of milk.\n'
+      '3 bottles of milk on the wall, '
+      '3 bottles of milk.\n'
       'Take one down and pass it around, '
-      '98 bottles of milk on the wall.\n'
+      '2 bottles of milk on the wall.\n'
     )