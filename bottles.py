sandimetz/99bottles_python

pass 2 test with outer conditional

bottles.py
@@ -1,5 +1,12 @@
 class Bottles:
   def verse(self, number):
+    if (number == 2):
+      return (
+        '2 bottles of beer on the wall, '
+        '2 bottles of beer.\n'
+        'Take one down and pass it around, '
+        '1 bottle of beer on the wall.\n'
+      )
     return (
       f'{number} bottles of beer on the wall, '
       f'{number} bottles of beer.\n'

sandimetz/99bottles_python_milk

bottles.py
@@ -1,5 +1,12 @@
 class Bottles:
   def verse(self, number):
+    if (number == 2):
+      return (
+        '2 bottles of milk on the wall, '
+        '2 bottles of milk.\n'
+        'Take one down and pass it around, '
+        '1 bottle of milk on the wall.\n'
+      )
     return (
       f'{number} bottles of milk on the wall, '
       f'{number} bottles of milk.\n'