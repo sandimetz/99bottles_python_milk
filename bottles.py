sandimetz/99bottles_python

interpolate number into verse string

bottles.py
@@ -1,13 +1,8 @@
 class Bottles:
   def verse(self, number):
-    if (number == 99):
-      n = 99
-    else:
-      n = 3
-
     return (
-      f'{n} bottles of beer on the wall, '
-      f'{n} bottles of beer.\n'
+      f'{number} bottles of beer on the wall, '
+      f'{number} bottles of beer.\n'
       f'Take one down and pass it around, '
-      f'{n-1} bottles of beer on the wall.\n'
+      f'{number-1} bottles of beer on the wall.\n'
     )

sandimetz/99bottles_python_milk

bottles.py
@@ -1,13 +1,8 @@
 class Bottles:
   def verse(self, number):
-    if (number == 99):
-      n = 99
-    else:
-      n = 3
-
     return (
-      f'{n} bottles of milk on the wall, '
-      f'{n} bottles of milk.\n'
+      f'{number} bottles of milk on the wall, '
+      f'{number} bottles of milk.\n'
       f'Take one down and pass it around, '
-      f'{n-1} bottles of milk on the wall.\n'
+      f'{number-1} bottles of milk on the wall.\n'
     )
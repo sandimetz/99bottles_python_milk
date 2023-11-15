beac00c1ed4bb68548d0c44a67410547b5e22130
Katrina Owen <katrina.owen@gmail.com>
2023-11-15 10:46:43 +0100

    interpolate number into verse string

--- bottles.py
+++ bottles.py
@@ -1,13 +1,8 @@
 class Bottles:
   def verse(self, number):
-    if (number == 99):
-      n = 99
-    else:
-      n = 3
-
     return (
-      f'{n} bottles of milk on the wall, '
-      f'{n} bottles of milk.\n'
+      f'{number} bottles of milk on the wall, '
+      f'{number} bottles of milk.\n'
       f'Take one down and pass it around, '
-      f'{n-1} bottles of milk on the wall.\n'
+      f'{number-1} bottles of milk on the wall.\n'
     )
