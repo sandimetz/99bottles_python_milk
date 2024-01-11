sandimetz/99bottles_python

revert prior change

bottles.py
@@ -26,17 +26,12 @@ class Bottles:
           f'{number} bottles of beer on the wall, '
           f'{number} bottles of beer.\n'
           f'Take one down and pass it around, '
-          f'{number-1} {self.container(number-1)} of beer on the wall.\n'
+          f'{number-1} bottle of beer on the wall.\n'
         )
       case _:
         return (
           f'{number} bottles of beer on the wall, '
           f'{number} bottles of beer.\n'
           f'Take one down and pass it around, '
-          f'{number-1} {self.container(number-1)} of beer on the wall.\n'
+          f'{number-1} bottles of beer on the wall.\n'
         )
-
-  def container(self, number):
-    if number == 1:
-      return 'bottle'
-    return 'bottles'

sandimetz/99bottles_python_milk

bottles.py
@@ -26,17 +26,12 @@ class Bottles:
           f'{number} bottles of milk on the wall, '
           f'{number} bottles of milk.\n'
           f'Take one down and pass it around, '
-          f'{number-1} {self.container(number-1)} of milk on the wall.\n'
+          f'{number-1} bottle of milk on the wall.\n'
         )
       case _:
         return (
           f'{number} bottles of milk on the wall, '
           f'{number} bottles of milk.\n'
           f'Take one down and pass it around, '
-          f'{number-1} {self.container(number-1)} of milk on the wall.\n'
+          f'{number-1} bottles of milk on the wall.\n'
         )
-
-  def container(self, number):
-    if number == 1:
-      return 'bottle'
-    return 'bottles'