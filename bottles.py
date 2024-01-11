sandimetz/99bottles_python

make phrase 2 of case 1 and default the same

bottles.py
@@ -17,14 +17,14 @@ class Bottles:
       case 1:
         return (
           f'{number} {self.container(number)} of beer on the wall, '
-          f'1 bottle of beer.\n'
+          f'{number} {self.container(number)} of beer.\n'
           f'Take it down and pass it around, '
           f'no more bottles of beer on the wall.\n'
         )
       case _:
         return (
           f'{number} {self.container(number)} of beer on the wall, '
-          f'{number} bottles of beer.\n'
+          f'{number} {self.container(number)} of beer.\n'
           f'Take one down and pass it around, '
           f'{number-1} {self.container(number-1)} of beer on the wall.\n'
         )

sandimetz/99bottles_python_milk

bottles.py
@@ -17,14 +17,14 @@ class Bottles:
       case 1:
         return (
           f'{number} {self.container(number)} of milk on the wall, '
-          f'1 bottle of milk.\n'
+          f'{number} {self.container(number)} of milk.\n'
           f'Take it down and pass it around, '
           f'no more bottles of milk on the wall.\n'
         )
       case _:
         return (
           f'{number} {self.container(number)} of milk on the wall, '
-          f'{number} bottles of milk.\n'
+          f'{number} {self.container(number)} of milk.\n'
           f'Take one down and pass it around, '
           f'{number-1} {self.container(number-1)} of milk on the wall.\n'
         )
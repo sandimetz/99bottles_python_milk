c7117e775d0af65cea5a8d5c3f8771a147695409
Katrina Owen <katrina.owen@gmail.com>
2024-01-04 11:15:13 +0100

    make phrase 2 of case 1 and default the same

--- bottles.py
+++ bottles.py
@@ -17,14 +17,14 @@ class Bottles:
       case 1:
         return (
           f'{number} {self.container(number)} of milk on the wall, '
-          f'1 bottle of milk.\n'
+          f'{number} {self.container(number)} of milk.\n'
           f'Take it down and pass it around, '
           f'no more bottles of milk on the wall.\n'
         )
       case _:
         return (
           f'{number} {self.container(number)} of milk on the wall, '
-          f'{number} bottles of milk.\n'
+          f'{number} {self.container(number)} of milk.\n'
           f'Take one down and pass it around, '
           f'{number-1} {self.container(number-1)} of milk on the wall.\n'
         )
