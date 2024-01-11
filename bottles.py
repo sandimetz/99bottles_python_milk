sandimetz/99bottles_python

interpolate number at start of 1 case

bottles.py
@@ -16,7 +16,7 @@ class Bottles:
         )
       case 1:
         return (
-          f'1 bottle of beer on the wall, '
+          f'{number} bottle of beer on the wall, '
           f'1 bottle of beer.\n'
           f'Take it down and pass it around, '
           f'no more bottles of beer on the wall.\n'

sandimetz/99bottles_python_milk

bottles.py
@@ -16,7 +16,7 @@ class Bottles:
         )
       case 1:
         return (
-          f'1 bottle of milk on the wall, '
+          f'{number} bottle of milk on the wall, '
           f'1 bottle of milk.\n'
           f'Take it down and pass it around, '
           f'no more bottles of milk on the wall.\n'