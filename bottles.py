sandimetz/99bottles_python

send quantity in case 0 phrase 1

bottles.py
@@ -9,7 +9,7 @@ class Bottles:
     match number:
       case 0:
         return (
-          f'No more bottles of beer on the wall, '
+          f'{self.quantity(number).capitalize()} bottles of beer on the wall, '
           f'no more bottles of beer.\n'
           f'Go to the store and buy some more, '
           f'99 bottles of beer on the wall.\n'

sandimetz/99bottles_python_milk

bottles.py
@@ -9,7 +9,7 @@ class Bottles:
     match number:
       case 0:
         return (
-          f'No more bottles of milk on the wall, '
+          f'{self.quantity(number).capitalize()} bottles of milk on the wall, '
           f'no more bottles of milk.\n'
           f'Go to the store and buy some more, '
           f'99 bottles of milk on the wall.\n'