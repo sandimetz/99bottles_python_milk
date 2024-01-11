sandimetz/99bottles_python

send quantity in phrase 4 of default case

bottles.py
@@ -26,7 +26,8 @@ class Bottles:
           f'{number} {self.container(number)} of beer on the wall, '
           f'{number} {self.container(number)} of beer.\n'
           f'Take {self.pronoun(number)} down and pass it around, '
-          f'{number-1} {self.container(number-1)} of beer on the wall.\n'
+          f'{self.quantity(number-1)} {self.container(number-1)}'
+            ' of beer on the wall.\n'
         )
 
   def quantity(self, number=0):

sandimetz/99bottles_python_milk

bottles.py
@@ -26,7 +26,8 @@ class Bottles:
           f'{number} {self.container(number)} of milk on the wall, '
           f'{number} {self.container(number)} of milk.\n'
           f'Take {self.pronoun(number)} down and pass it around, '
-          f'{number-1} {self.container(number-1)} of milk on the wall.\n'
+          f'{self.quantity(number-1)} {self.container(number-1)}'
+          ' of milk on the wall.\n'
         )
 
   def quantity(self, number=0):