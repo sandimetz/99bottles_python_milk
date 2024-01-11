sandimetz/99bottles_python

use #container in 2 case

bottles.py
@@ -26,7 +26,7 @@ class Bottles:
           f'{number} bottles of beer on the wall, '
           f'{number} bottles of beer.\n'
           f'Take one down and pass it around, '
-          f'{number-1} bottle of beer on the wall.\n'
+          f'{number-1} {self.container(number-1)} of beer on the wall.\n'
         )
       case _:
         return (

sandimetz/99bottles_python_milk

bottles.py
@@ -26,7 +26,7 @@ class Bottles:
           f'{number} bottles of milk on the wall, '
           f'{number} bottles of milk.\n'
           f'Take one down and pass it around, '
-          f'{number-1} bottle of milk on the wall.\n'
+          f'{number-1} {self.container(number-1)} of milk on the wall.\n'
         )
       case _:
         return (