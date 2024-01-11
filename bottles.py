sandimetz/99bottles_python

pass arg when sending #container

bottles.py
@@ -33,7 +33,7 @@ class Bottles:
           f'{number} bottles of beer on the wall, '
           f'{number} bottles of beer.\n'
           f'Take one down and pass it around, '
-          f'{number-1} {self.container()} of beer on the wall.\n'
+          f'{number-1} {self.container(number-1)} of beer on the wall.\n'
         )
 
   def container(self, number='FIXME'):

sandimetz/99bottles_python_milk

bottles.py
@@ -33,7 +33,7 @@ class Bottles:
           f'{number} bottles of milk on the wall, '
           f'{number} bottles of milk.\n'
           f'Take one down and pass it around, '
-          f'{number-1} {self.container()} of milk on the wall.\n'
+          f'{number-1} {self.container(number-1)} of milk on the wall.\n'
         )
 
   def container(self, number='FIXME'):