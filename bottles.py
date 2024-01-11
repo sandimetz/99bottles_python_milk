sandimetz/99bottles_python

make case 0 f-strings

bottles.py
@@ -9,10 +9,10 @@ class Bottles:
     match number:
       case 0:
         return (
-          'No more bottles of beer on the wall, '
-          'no more bottles of beer.\n'
-          'Go to the store and buy some more, '
-          '99 bottles of beer on the wall.\n'
+          f'No more bottles of beer on the wall, '
+          f'no more bottles of beer.\n'
+          f'Go to the store and buy some more, '
+          f'99 bottles of beer on the wall.\n'
         )
       case _:
         return (

sandimetz/99bottles_python_milk

bottles.py
@@ -9,10 +9,10 @@ class Bottles:
     match number:
       case 0:
         return (
-          'No more bottles of milk on the wall, '
-          'no more bottles of milk.\n'
-          'Go to the store and buy some more, '
-          '99 bottles of milk on the wall.\n'
+          f'No more bottles of milk on the wall, '
+          f'no more bottles of milk.\n'
+          f'Go to the store and buy some more, '
+          f'99 bottles of milk on the wall.\n'
         )
       case _:
         return (