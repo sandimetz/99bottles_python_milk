sandimetz/99bottles_python

1 case sends quantity

bottles.py
@@ -19,7 +19,7 @@ class Bottles:
           f'{number} {self.container(number)} of beer on the wall, '
           f'{number} {self.container(number)} of beer.\n'
           f'Take {self.pronoun(number)} down and pass it around, '
-          f'no more bottles of beer on the wall.\n'
+          f'{self.quantity()} bottles of beer on the wall.\n'
         )
       case _:
         return (

sandimetz/99bottles_python_milk

bottles.py
@@ -19,7 +19,7 @@ class Bottles:
           f'{number} {self.container(number)} of milk on the wall, '
           f'{number} {self.container(number)} of milk.\n'
           f'Take {self.pronoun(number)} down and pass it around, '
-          f'no more bottles of milk on the wall.\n'
+          f'{self.quantity()} bottles of milk on the wall.\n'
         )
       case _:
         return (