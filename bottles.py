sandimetz/99bottles_python

interpolate number in 2 case

bottles.py
@@ -23,10 +23,10 @@ class Bottles:
         )
       case 2:
         return (
-          '2 bottles of beer on the wall, '
-          '2 bottles of beer.\n'
-          'Take one down and pass it around, '
-          '1 bottle of beer on the wall.\n'
+          f'{number} bottles of beer on the wall, '
+          f'{number} bottles of beer.\n'
+          f'Take one down and pass it around, '
+          f'{number-1} bottle of beer on the wall.\n'
         )
       case _:
         return (

sandimetz/99bottles_python_milk

bottles.py
@@ -23,10 +23,10 @@ class Bottles:
         )
       case 2:
         return (
-          '2 bottles of milk on the wall, '
-          '2 bottles of milk.\n'
-          'Take one down and pass it around, '
-          '1 bottle of milk on the wall.\n'
+          f'{number} bottles of milk on the wall, '
+          f'{number} bottles of milk.\n'
+          f'Take one down and pass it around, '
+          f'{number-1} bottle of milk on the wall.\n'
         )
       case _:
         return (
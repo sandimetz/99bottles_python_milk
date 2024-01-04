7905d3afa4653d305829ec9608499a287ab6fd1b
Katrina Owen <katrina.owen@gmail.com>
2024-01-04 08:32:54 +0100

    interpolate number in 2 case

--- bottles.py
+++ bottles.py
@@ -23,10 +23,10 @@ class Bottles:
         )
       case 2:
         return (
-          '2 bottles of milk on the wall, '
-          '2 bottles of milk.\n'
-          'Take one down and pass it around, '
-          '1 bottle of milk on the wall.\n'
+          f'{number} bottles of milk on the wall, '
+          f'{number} bottles of milk.\n'
+          f'Take one down and pass it around, '
+          f'{number-1} bottle of milk on the wall.\n'
         )
       case _:
         return (
