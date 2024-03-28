44cf875e7c1d0290ccfb59544a5f8cfac0a0235a
Katrina Owen <katrina.owen@gmail.com>
2024-03-28 07:56:37 +0100

    put back bottle-number 6 logic

--- bottles.py
+++ bottles.py
@@ -23,6 +23,8 @@ class BottleNumber:
         cls = BottleNumber0
       case 1:
         cls = BottleNumber1
+      case 6:
+        cls = BottleNumber6
       case _:
         cls = BottleNumber
     return super().__new__(cls)
