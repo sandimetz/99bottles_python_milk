9864f00d9414c0f0d15e09456e5917bb13f6e2b1
Katrina Owen <katrina.owen@gmail.com>
2024-03-30 10:14:41 +0100

    make _lyrics work with both numbers and bottle_numbers

--- bottles.py
+++ bottles.py
@@ -8,7 +8,8 @@ class BottleVerse:
     self._number = number
 
   def _lyrics(self):
-    bottle_number = BottleNumber(self._number)
+    bottle_number = (self._number if isinstance(self._number, BottleNumber)
+                     else BottleNumber(self._number))
 
     return (
       f'{bottle_number} of milk on the wall, '.capitalize() +
