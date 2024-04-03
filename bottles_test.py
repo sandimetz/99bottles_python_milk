7b067813144abe32010e8e101ab19f83e2499e1a
Katrina Owen <katrina.owen@gmail.com>
2024-04-03 10:23:46 +0200

    delete old test for verse 99

--- bottles_test.py
+++ bottles_test.py
@@ -14,15 +14,6 @@ class TestBottleVerse:
 
 
 class TestBottles:
-  def test_the_first_verse(self):
-    expected = (
-      '99 bottles of milk on the wall, '
-      '99 bottles of milk.\n'
-      'Take one down and pass it around, '
-      '98 bottles of milk on the wall.\n'
-    )
-    assert Bottles().verse(99) == expected
-
   def test_another_verse(self):
     expected = (
       '3 bottles of milk on the wall, '
