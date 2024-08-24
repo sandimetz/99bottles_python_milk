sandimetz/99bottles_python

consolidate verse phrase lines

bottles.py
@@ -10,12 +10,10 @@ class Bottles:
     next_bottle_number = BottleNumber(bottle_number.successor())
 
     return (
-      f'{bottle_number}'.capitalize() +
-        ' of beer on the wall, '
+      f'{bottle_number} of beer on the wall, '.capitalize() +
       f'{bottle_number} of beer.\n'
       f'{bottle_number.action()}, '
-      f'{next_bottle_number}'
-        ' of beer on the wall.\n'
+      f'{next_bottle_number} of beer on the wall.\n'
     )
 
 class BottleNumber:

sandimetz/99bottles_python_milk

bottles.py
@@ -10,12 +10,10 @@ class Bottles:
     next_bottle_number = BottleNumber(bottle_number.successor())
 
     return (
-      f'{bottle_number}'.capitalize() +
-      ' of milk on the wall, '
+      f'{bottle_number} of milk on the wall, '.capitalize() +
       f'{bottle_number} of milk.\n'
       f'{bottle_number.action()}, '
-      f'{next_bottle_number}'
-      ' of milk on the wall.\n'
+      f'{next_bottle_number} of milk on the wall.\n'
     )
 
 