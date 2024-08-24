sandimetz/99bottles_python

encapsulate bottles tests in TestBottles class

bottles_test.py
@@ -1,93 +1,88 @@
+import pytest
 from bottles import Bottles
 
 
-def test_the_first_verse():
-  expected = (
-    '99 bottles of beer on the wall, '
-    '99 bottles of beer.\n'
-    'Take one down and pass it around, '
-    '98 bottles of beer on the wall.\n'
-  )
-  assert Bottles().verse(99) == expected
+class TestBottles:
+  def test_the_first_verse(self):
+    expected = (
+      '99 bottles of beer on the wall, '
+      '99 bottles of beer.\n'
+      'Take one down and pass it around, '
+      '98 bottles of beer on the wall.\n'
+    )
+    assert Bottles().verse(99) == expected
 
+  def test_another_verse(self):
+    expected = (
+      '3 bottles of beer on the wall, '
+      '3 bottles of beer.\n'
+      'Take one down and pass it around, '
+      '2 bottles of beer on the wall.\n'
+    )
+    assert Bottles().verse(3) == expected
 
-def test_another_verse():
-  expected = (
-    '3 bottles of beer on the wall, '
-    '3 bottles of beer.\n'
-    'Take one down and pass it around, '
-    '2 bottles of beer on the wall.\n'
-  )
-  assert Bottles().verse(3) == expected
+  def test_verse_2(self):
+    expected = (
+      '2 bottles of beer on the wall, '
+      '2 bottles of beer.\n'
+      'Take one down and pass it around, '
+      '1 bottle of beer on the wall.\n'
+    )
+    assert Bottles().verse(2) == expected
 
+  def test_verse_1(self):
+    expected = (
+      '1 bottle of beer on the wall, '
+      '1 bottle of beer.\n'
+      'Take it down and pass it around, '
+      'no more bottles of beer on the wall.\n'
+    )
+    assert Bottles().verse(1) == expected
 
-def test_verse_2():
-  expected = (
-    '2 bottles of beer on the wall, '
-    '2 bottles of beer.\n'
-    'Take one down and pass it around, '
-    '1 bottle of beer on the wall.\n'
-  )
-  assert Bottles().verse(2) == expected
+  def test_verse_0(self):
+    expected = (
+      'No more bottles of beer on the wall, '
+      'no more bottles of beer.\n'
+      'Go to the store and buy some more, '
+      '99 bottles of beer on the wall.\n'
+    )
+    assert Bottles().verse(0) == expected
 
+  def test_a_couple_verses(self):
+    expected = (
+      '99 bottles of beer on the wall, '
+      '99 bottles of beer.\n'
+      'Take one down and pass it around, '
+      '98 bottles of beer on the wall.\n'
+      '\n'
+      '98 bottles of beer on the wall, '
+      '98 bottles of beer.\n'
+      'Take one down and pass it around, '
+      '97 bottles of beer on the wall.\n'
+    )
+    assert Bottles().verses(99, 98) == expected
 
-def test_verse_1():
-  expected = (
-    '1 bottle of beer on the wall, '
-    '1 bottle of beer.\n'
-    'Take it down and pass it around, '
-    'no more bottles of beer on the wall.\n'
-  )
-  assert Bottles().verse(1) == expected
+  def test_a_few_verses(self):
+    expected = (
+      '2 bottles of beer on the wall, '
+      '2 bottles of beer.\n'
+      'Take one down and pass it around, '
+      '1 bottle of beer on the wall.\n'
+      '\n'
+      '1 bottle of beer on the wall, '
+      '1 bottle of beer.\n'
+      'Take it down and pass it around, '
+      'no more bottles of beer on the wall.\n'
+      '\n'
+      'No more bottles of beer on the wall, '
+      'no more bottles of beer.\n'
+      'Go to the store and buy some more, '
+      '99 bottles of beer on the wall.\n'
+    )
+    assert Bottles().verses(2, 0) == expected
 
-
-def test_verse_0():
-  expected = (
-    'No more bottles of beer on the wall, '
-    'no more bottles of beer.\n'
-    'Go to the store and buy some more, '
-    '99 bottles of beer on the wall.\n'
-  )
-  assert Bottles().verse(0) == expected
-
-
-def test_a_couple_verses():
-  expected = (
-    '99 bottles of beer on the wall, '
-    '99 bottles of beer.\n'
-    'Take one down and pass it around, '
-    '98 bottles of beer on the wall.\n'
-    '\n'
-    '98 bottles of beer on the wall, '
-    '98 bottles of beer.\n'
-    'Take one down and pass it around, '
-    '97 bottles of beer on the wall.\n'
-  )
-  assert Bottles().verses(99, 98) == expected
-
-
-def test_a_few_verses():
-  expected = (
-    '2 bottles of beer on the wall, '
-    '2 bottles of beer.\n'
-    'Take one down and pass it around, '
-    '1 bottle of beer on the wall.\n'
-    '\n'
-    '1 bottle of beer on the wall, '
-    '1 bottle of beer.\n'
-    'Take it down and pass it around, '
-    'no more bottles of beer on the wall.\n'
-    '\n'
-    'No more bottles of beer on the wall, '
-    'no more bottles of beer.\n'
-    'Go to the store and buy some more, '
-    '99 bottles of beer on the wall.\n'
-  )
-  assert Bottles().verses(2, 0) == expected
-
-
-def test_the_whole_song():
-  expected = """99 bottles of beer on the wall, 99 bottles of beer.
+  def test_the_whole_song(self):
+    expected = """99 bottles of beer on the wall, 99 bottles of beer.
 Take one down and pass it around, 98 bottles of beer on the wall.
 
 98 bottles of beer on the wall, 98 bottles of beer.
@@ -387,4 +382,4 @@ Take it down and pass it around, no more bottles of beer on the wall.
 No more bottles of beer on the wall, no more bottles of beer.
 Go to the store and buy some more, 99 bottles of beer on the wall.
 """
-  assert Bottles().song() == expected
+    assert Bottles().song() == expected

sandimetz/99bottles_python_milk

bottles_test.py
@@ -1,93 +1,88 @@
+import pytest
 from bottles import Bottles
 
 
-def test_the_first_verse():
-  expected = (
-    '99 bottles of milk on the wall, '
-    '99 bottles of milk.\n'
-    'Take one down and pass it around, '
-    '98 bottles of milk on the wall.\n'
-  )
-  assert Bottles().verse(99) == expected
+class TestBottles:
+  def test_the_first_verse(self):
+    expected = (
+      '99 bottles of milk on the wall, '
+      '99 bottles of milk.\n'
+      'Take one down and pass it around, '
+      '98 bottles of milk on the wall.\n'
+    )
+    assert Bottles().verse(99) == expected
 
+  def test_another_verse(self):
+    expected = (
+      '3 bottles of milk on the wall, '
+      '3 bottles of milk.\n'
+      'Take one down and pass it around, '
+      '2 bottles of milk on the wall.\n'
+    )
+    assert Bottles().verse(3) == expected
 
-def test_another_verse():
-  expected = (
-    '3 bottles of milk on the wall, '
-    '3 bottles of milk.\n'
-    'Take one down and pass it around, '
-    '2 bottles of milk on the wall.\n'
-  )
-  assert Bottles().verse(3) == expected
+  def test_verse_2(self):
+    expected = (
+      '2 bottles of milk on the wall, '
+      '2 bottles of milk.\n'
+      'Take one down and pass it around, '
+      '1 bottle of milk on the wall.\n'
+    )
+    assert Bottles().verse(2) == expected
 
+  def test_verse_1(self):
+    expected = (
+      '1 bottle of milk on the wall, '
+      '1 bottle of milk.\n'
+      'Take it down and pass it around, '
+      'no more bottles of milk on the wall.\n'
+    )
+    assert Bottles().verse(1) == expected
 
-def test_verse_2():
-  expected = (
-    '2 bottles of milk on the wall, '
-    '2 bottles of milk.\n'
-    'Take one down and pass it around, '
-    '1 bottle of milk on the wall.\n'
-  )
-  assert Bottles().verse(2) == expected
+  def test_verse_0(self):
+    expected = (
+      'No more bottles of milk on the wall, '
+      'no more bottles of milk.\n'
+      'Go to the store and buy some more, '
+      '99 bottles of milk on the wall.\n'
+    )
+    assert Bottles().verse(0) == expected
 
+  def test_a_couple_verses(self):
+    expected = (
+      '99 bottles of milk on the wall, '
+      '99 bottles of milk.\n'
+      'Take one down and pass it around, '
+      '98 bottles of milk on the wall.\n'
+      '\n'
+      '98 bottles of milk on the wall, '
+      '98 bottles of milk.\n'
+      'Take one down and pass it around, '
+      '97 bottles of milk on the wall.\n'
+    )
+    assert Bottles().verses(99, 98) == expected
 
-def test_verse_1():
-  expected = (
-    '1 bottle of milk on the wall, '
-    '1 bottle of milk.\n'
-    'Take it down and pass it around, '
-    'no more bottles of milk on the wall.\n'
-  )
-  assert Bottles().verse(1) == expected
+  def test_a_few_verses(self):
+    expected = (
+      '2 bottles of milk on the wall, '
+      '2 bottles of milk.\n'
+      'Take one down and pass it around, '
+      '1 bottle of milk on the wall.\n'
+      '\n'
+      '1 bottle of milk on the wall, '
+      '1 bottle of milk.\n'
+      'Take it down and pass it around, '
+      'no more bottles of milk on the wall.\n'
+      '\n'
+      'No more bottles of milk on the wall, '
+      'no more bottles of milk.\n'
+      'Go to the store and buy some more, '
+      '99 bottles of milk on the wall.\n'
+    )
+    assert Bottles().verses(2, 0) == expected
 
-
-def test_verse_0():
-  expected = (
-    'No more bottles of milk on the wall, '
-    'no more bottles of milk.\n'
-    'Go to the store and buy some more, '
-    '99 bottles of milk on the wall.\n'
-  )
-  assert Bottles().verse(0) == expected
-
-
-def test_a_couple_verses():
-  expected = (
-    '99 bottles of milk on the wall, '
-    '99 bottles of milk.\n'
-    'Take one down and pass it around, '
-    '98 bottles of milk on the wall.\n'
-    '\n'
-    '98 bottles of milk on the wall, '
-    '98 bottles of milk.\n'
-    'Take one down and pass it around, '
-    '97 bottles of milk on the wall.\n'
-  )
-  assert Bottles().verses(99, 98) == expected
-
-
-def test_a_few_verses():
-  expected = (
-    '2 bottles of milk on the wall, '
-    '2 bottles of milk.\n'
-    'Take one down and pass it around, '
-    '1 bottle of milk on the wall.\n'
-    '\n'
-    '1 bottle of milk on the wall, '
-    '1 bottle of milk.\n'
-    'Take it down and pass it around, '
-    'no more bottles of milk on the wall.\n'
-    '\n'
-    'No more bottles of milk on the wall, '
-    'no more bottles of milk.\n'
-    'Go to the store and buy some more, '
-    '99 bottles of milk on the wall.\n'
-  )
-  assert Bottles().verses(2, 0) == expected
-
-
-def test_the_whole_song():
-  expected = """99 bottles of milk on the wall, 99 bottles of milk.
+  def test_the_whole_song(self):
+    expected = """99 bottles of milk on the wall, 99 bottles of milk.
 Take one down and pass it around, 98 bottles of milk on the wall.
 
 98 bottles of milk on the wall, 98 bottles of milk.
@@ -387,4 +382,4 @@ Take it down and pass it around, no more bottles of milk on the wall.
 No more bottles of milk on the wall, no more bottles of milk.
 Go to the store and buy some more, 99 bottles of milk on the wall.
 """
-  assert Bottles().song() == expected
+    assert Bottles().song() == expected