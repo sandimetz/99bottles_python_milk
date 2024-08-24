sandimetz/99bottles_python

add constructor and store property

bottles.py
@@ -41,6 +41,9 @@ class Bottles:
     return number - 1
 
 class BottleNumber:
+  def __init__(self, number):
+    self.number = number
+
   def quantity(self, number):
     if number == 0:
       return 'no more'

sandimetz/99bottles_python_milk

bottles.py
@@ -43,6 +43,9 @@ class Bottles:
 
 
 class BottleNumber:
+  def __init__(self, number):
+    self._number = number
+
   def quantity(self, number):
     if number == 0:
       return 'no more'