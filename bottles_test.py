sandimetz/99bottles_python

copy test_the_first_verse into TestBottleVerse class

bottles_test.py
@@ -3,7 +3,14 @@ from bottles import Bottles
 
 
 class TestBottleVerse:
-  pass
+  def test_the_first_verse(self):
+    expected = (
+      '99 bottles of beer on the wall, '
+      '99 bottles of beer.\n'
+      'Take one down and pass it around, '
+      '98 bottles of beer on the wall.\n'
+    )
+    assert Bottles().verse(99) == expected
 
 
 class TestBottles:

sandimetz/99bottles_python_milk

bottles_test.py
@@ -3,7 +3,14 @@ from bottles import Bottles
 
 
 class TestBottleVerse:
-  pass
+  def test_the_first_verse(self):
+    expected = (
+      '99 bottles of milk on the wall, '
+      '99 bottles of milk.\n'
+      'Take one down and pass it around, '
+      '98 bottles of milk on the wall.\n'
+    )
+    assert Bottles().verse(99) == expected
 
 
 class TestBottles:
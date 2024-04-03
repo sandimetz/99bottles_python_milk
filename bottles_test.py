e4fb13f3e566f6f649db0ae22200ee81a593b4fc
Katrina Owen <katrina.owen@gmail.com>
2024-04-03 10:17:50 +0200

    copy test_the_first_verse into TestBottleVerse class

--- bottles_test.py
+++ bottles_test.py
@@ -3,7 +3,14 @@ from bottles import Bottles
 
 
 class TestBottleVerse:
-  pass
+  def test_the_first_verse(self):
+    expected = (
+      '99 bottles of milk on the wall, '
+      '99 bottles of milk.\n'
+      'Take one down and pass it around, '
+      '98 bottles of milk on the wall.\n'
+    )
+    assert Bottles().verse(99) == expected
 
 
 class TestBottles:
