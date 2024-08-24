sandimetz/99bottles_python

send quantity with 99 as arg

bottles.py
@@ -13,7 +13,7 @@ class Bottles:
             ' of beer on the wall, '
           f'{self.quantity(number)} {self.container(number)} of beer.\n'
           f'{self.action(number)}, '
-          f'99 bottles of beer on the wall.\n'
+          f'{self.quantity(99)} bottles of beer on the wall.\n'
         )
       case _:
         return (

sandimetz/99bottles_python_milk

bottles.py
@@ -13,7 +13,7 @@ class Bottles:
           ' of milk on the wall, '
           f'{self.quantity(number)} {self.container(number)} of milk.\n'
           f'{self.action(number)}, '
-          f'99 bottles of milk on the wall.\n'
+          f'{self.quantity(99)} bottles of milk on the wall.\n'
         )
       case _:
         return (