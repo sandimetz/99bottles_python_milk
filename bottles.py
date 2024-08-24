sandimetz/99bottles_python

revert to consistent flocked methods

bottles.py
@@ -13,7 +13,7 @@ class Bottles:
             ' of beer on the wall, '
           f'{self.quantity(number)} {self.container(number)} of beer.\n'
           f'{self.action(number)}, '
-          f'{self.quantity(number-1)} bottles of beer on the wall.\n'
+          f'99 bottles of beer on the wall.\n'
         )
       case _:
         return (
@@ -26,13 +26,9 @@ class Bottles:
         )
 
   def quantity(self, number):
-    match number:
-      case -1:
-        return "99"
-      case 0:
-        return 'no more'
-      case _:
-        return str(number)
+    if number == 0:
+      return 'no more'
+    return str(number)
 
   def container(self, number):
     if number == 1:

sandimetz/99bottles_python_milk

bottles.py
@@ -13,7 +13,7 @@ class Bottles:
           ' of milk on the wall, '
           f'{self.quantity(number)} {self.container(number)} of milk.\n'
           f'{self.action(number)}, '
-          f'{self.quantity(number-1)} bottles of milk on the wall.\n'
+          f'99 bottles of milk on the wall.\n'
         )
       case _:
         return (
@@ -26,13 +26,9 @@ class Bottles:
         )
 
   def quantity(self, number):
-    match number:
-      case -1:
-        return "99"
-      case 0:
-        return 'no more'
-      case _:
-        return str(number)
+    if number == 0:
+      return 'no more'
+    return str(number)
 
   def container(self, number):
     if number == 1: