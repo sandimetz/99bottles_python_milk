sandimetz/99bottles_python

return bottle number object from successor in superclass

bottles.py
@@ -51,7 +51,7 @@ class BottleNumber:
     return 'one'
 
   def successor(self):
-    return self.number - 1
+    return BottleNumber.given(self.number - 1)
 
 class BottleNumber0(BottleNumber):
   def quantity(self):

sandimetz/99bottles_python_milk

bottles.py
@@ -52,7 +52,7 @@ class BottleNumber:
     return 'one'
 
   def successor(self):
-    return self._number - 1
+    return BottleNumber.given(self._number - 1)
 
 
 class BottleNumber0(BottleNumber):