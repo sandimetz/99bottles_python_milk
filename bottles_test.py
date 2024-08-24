sandimetz/99bottles_python

change test to reflect six-pack requirement FAILS

bottles_test.py
@@ -359,9 +359,9 @@ Take one down and pass it around, 8 bottles of beer on the wall.
 Take one down and pass it around, 7 bottles of beer on the wall.
 
 7 bottles of beer on the wall, 7 bottles of beer.
-Take one down and pass it around, 6 bottles of beer on the wall.
+Take one down and pass it around, 1 six-pack of beer on the wall.
 
-6 bottles of beer on the wall, 6 bottles of beer.
+1 six-pack of beer on the wall, 1 six-pack of beer.
 Take one down and pass it around, 5 bottles of beer on the wall.
 
 5 bottles of beer on the wall, 5 bottles of beer.

sandimetz/99bottles_python_milk

bottles_test.py
@@ -364,9 +364,9 @@ Take one down and pass it around, 8 bottles of milk on the wall.
 Take one down and pass it around, 7 bottles of milk on the wall.
 
 7 bottles of milk on the wall, 7 bottles of milk.
-Take one down and pass it around, 6 bottles of milk on the wall.
+Take one down and pass it around, 1 six-pack of milk on the wall.
 
-6 bottles of milk on the wall, 6 bottles of milk.
+1 six-pack of milk on the wall, 1 six-pack of milk.
 Take one down and pass it around, 5 bottles of milk on the wall.
 
 5 bottles of milk on the wall, 5 bottles of milk.