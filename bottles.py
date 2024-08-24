sandimetz/99bottles_python

refer to bottle number property in _lyrics method

bottles.py
@@ -11,10 +11,10 @@ class BottleVerse:
     bottle_number = self._number
 
     return (
-      f'{bottle_number} of beer on the wall, '.capitalize() +
-      f'{bottle_number} of beer.\n'
-      f'{bottle_number.action()}, '
-      f'{bottle_number.successor()} of beer on the wall.\n'
+      f'{self._bottle_number} of beer on the wall, '.capitalize() +
+      f'{self._bottle_number} of beer.\n'
+      f'{self._bottle_number.action()}, '
+      f'{self._bottle_number.successor()} of beer on the wall.\n'
     )
 
 

sandimetz/99bottles_python_milk

bottles.py
@@ -11,10 +11,10 @@ class BottleVerse:
     bottle_number = self._number
 
     return (
-      f'{bottle_number} of milk on the wall, '.capitalize() +
-      f'{bottle_number} of milk.\n'
-      f'{bottle_number.action()}, '
-      f'{bottle_number.successor()} of milk on the wall.\n'
+      f'{self._bottle_number} of milk on the wall, '.capitalize() +
+      f'{self._bottle_number} of milk.\n'
+      f'{self._bottle_number.action()}, '
+      f'{self._bottle_number.successor()} of milk on the wall.\n'
     )
 
 